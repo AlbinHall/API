60297338f474acc71d84bd7c31207b9403f3b440
AlbinHall <albin-hall1@hotmail.com>
2022-11-13 22:20:44 +0000

    add display exceptions funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,24 @@ document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1])
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join())
+
+    return form;
+}
+
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -22,9 +38,11 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayExceptions(data)
         throw new Error(data.error);
     }
 }
+
 
 function displayErrors(data) {
     let heading = `JShint results for ${data.file}`
@@ -57,6 +75,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayExceptions(data);
         throw new Error(data.error);
     }
 
@@ -71,4 +90,17 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+}
+
+function displayExceptions(data) {
+    let heading = "An Exception Occured";
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error Number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <Strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show()
+
 }
